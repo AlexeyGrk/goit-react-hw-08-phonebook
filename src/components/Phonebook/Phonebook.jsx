fbefd093d5eac6c546369fde69dea28d312c75cb
AlexeyGrk <alexgmirko@gmail.com>
2021-07-15 10:04:14 +0300

    add localstorage functional

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -13,22 +13,30 @@ import {
 
 export class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
     name: "",
     number: "",
   };
+
   henleDobleNameAndNumber = (e) => {
     this.setState({
       [e.currentTarget.name]: e.currentTarget.value,
     });
   };
-
+  componentDidMount() {
+    const contactsParse = JSON.parse(localStorage.getItem("contacts"));
+    if (contactsParse) {
+      this.setState({
+        contacts: contactsParse,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
   handleNameFilter = (e) => {
     this.setState({
       filter: e.target.value,
